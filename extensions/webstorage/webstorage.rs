e50d3116e6bf105350205e07489e765f554ba2db
crowlkats <13135287+crowlKats@users.noreply.github.com>
2021-05-29 21:30:17 +0200

    move webstorage to own file

---
+++ extensions/webstorage/webstorage.rs
@@ -0,0 +1,177 @@
+use deno_core::error::AnyError;
+use deno_core::OpState;
+use rusqlite::params;
+use rusqlite::Connection;
+use rusqlite::OptionalExtension;
+use serde::Deserialize;
+
+struct LocalStorage(Connection);
+struct SessionStorage(Connection);
+
+fn get_webstorage(
+  state: &mut OpState,
+  persistent: bool,
+) -> Result<&Connection, AnyError> {
+  let conn = if persistent {
+    if state.try_borrow::<LocalStorage>().is_none() {
+      let path =
+        state
+          .try_borrow::<super::OriginStorageDir>()
+          .ok_or_else(|| {
+            super::DomExceptionNotSupportedError::new(
+              "LocalStorage is not supported in this context.",
+            )
+          })?;
+      std::fs::create_dir_all(&path.0)?;
+      let conn = Connection::open(path.0.join("local_storage"))?;
+      conn.execute(
+        "CREATE TABLE IF NOT EXISTS data (key VARCHAR UNIQUE, value VARCHAR)",
+        params![],
+      )?;
+
+      state.put(LocalStorage(conn));
+    }
+
+    &state.borrow::<LocalStorage>().0
+  } else {
+    if state.try_borrow::<SessionStorage>().is_none() {
+      let conn = Connection::open_in_memory()?;
+      conn.execute(
+        "CREATE TABLE data (key VARCHAR UNIQUE, value VARCHAR)",
+        params![],
+      )?;
+
+      state.put(SessionStorage(conn));
+    }
+
+    &state.borrow::<SessionStorage>().0
+  };
+
+  Ok(conn)
+}
+
+pub fn op_webstorage_length(
+  state: &mut OpState,
+  persistent: bool,
+  _: (),
+) -> Result<u32, AnyError> {
+  let conn = get_webstorage(state, persistent)?;
+
+  let mut stmt = conn.prepare("SELECT COUNT(*) FROM data")?;
+
+  let length: u32 = stmt.query_row(params![], |row| row.get(0))?;
+
+  Ok(length)
+}
+
+pub fn op_webstorage_key(
+  state: &mut OpState,
+  index: u32,
+  persistent: bool,
+) -> Result<Option<String>, AnyError> {
+  let conn = get_webstorage(state, persistent)?;
+
+  let mut stmt = conn.prepare("SELECT key FROM data LIMIT 1 OFFSET ?")?;
+
+  let key: Option<String> = stmt
+    .query_row(params![index], |row| row.get(0))
+    .optional()?;
+
+  Ok(key)
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SetArgs {
+  key_name: String,
+  key_value: String,
+}
+
+pub fn op_webstorage_set(
+  state: &mut OpState,
+  args: SetArgs,
+  persistent: bool,
+) -> Result<(), AnyError> {
+  let conn = get_webstorage(state, persistent)?;
+
+  let mut stmt =
+    conn.prepare("SELECT SUM(pgsize) FROM dbstat WHERE name = 'data'")?;
+  let size: u32 = stmt.query_row(params![], |row| row.get(0))?;
+
+  if size >= 5000000 {
+    return Err(
+      super::DomExceptionQuotaExceededError::new(
+        "Exceeded maximum storage size",
+      )
+      .into(),
+    );
+  }
+
+  conn.execute(
+    "INSERT OR REPLACE INTO data (key, value) VALUES (?, ?)",
+    params![args.key_name, args.key_value],
+  )?;
+
+  Ok(())
+}
+
+pub fn op_webstorage_get(
+  state: &mut OpState,
+  key_name: String,
+  persistent: bool,
+) -> Result<Option<String>, AnyError> {
+  let conn = get_webstorage(state, persistent)?;
+
+  let mut stmt = conn.prepare("SELECT value FROM data WHERE key = ?")?;
+
+  let val = stmt
+    .query_row(params![key_name], |row| row.get(0))
+    .optional()?;
+
+  Ok(val)
+}
+
+pub fn op_webstorage_remove(
+  state: &mut OpState,
+  key_name: String,
+  persistent: bool,
+) -> Result<(), AnyError> {
+  let conn = get_webstorage(state, persistent)?;
+
+  conn.execute("DELETE FROM data WHERE key = ?", params![key_name])?;
+
+  Ok(())
+}
+
+pub fn op_webstorage_clear(
+  state: &mut OpState,
+  persistent: bool,
+  _: (),
+) -> Result<(), AnyError> {
+  let conn = get_webstorage(state, persistent)?;
+
+  conn.execute("DROP TABLE data", params![])?;
+  conn.execute(
+    "CREATE TABLE data (key VARCHAR UNIQUE, value VARCHAR)",
+    params![],
+  )?;
+
+  Ok(())
+}
+
+pub fn op_webstorage_iterate_keys(
+  state: &mut OpState,
+  persistent: bool,
+  _: (),
+) -> Result<Vec<String>, AnyError> {
+  let conn = get_webstorage(state, persistent)?;
+
+  let mut stmt = conn.prepare("SELECT key FROM data")?;
+
+  let keys = stmt
+    .query_map(params![], |row| row.get::<_, String>(0))?
+    .map(|r| r.unwrap())
+    .collect();
+
+  Ok(keys)
+}
